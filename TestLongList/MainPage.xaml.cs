0935545901e975f66cb48ec5bac27ae18b7830ab
Dushyant Bansal <dushyant37@gmail.com>
2013-07-01 21:25:18 +0530

    first commit - basic project using longlist selector - pull to refresh working

---
+++ TestLongList/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TestLongList.Resources;
+using TestLongList.ViewModels;
+
+namespace TestLongList
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private bool _firstRealized;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+        private void AddTop(object sender, EventArgs e)
+        {
+            App.ViewModel.LoadTopElements(10);
+        }
+
+        private void AddBottom(object sender, EventArgs e)
+        {
+            App.ViewModel.LoadBottomElements();
+        }
+
+        private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //App.ViewModel.SetPullDetector(ItemsList);
+            App.ViewModel.LoadTopElements(26);
+        }
+
+        //private void LongListSelector_OnItemRealized(object sender, ItemRealizationEventArgs e)
+        //{
+        //    if (e.ItemKind != LongListSelectorItemKind.Item)
+        //        return;
+        //    var item = e.Container.Content as ItemViewModel;
+        //    if (!App.ViewModel.IsDataLoading && ItemsList.ItemsSource[0] == item )
+        //    {
+        //        App.ViewModel.LoadTopElements();
+        //    }
+        //}
+
+        private void ItemsList_OnManipulationStateChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void ItemsList_OnItemRealized(object sender, ItemRealizationEventArgs e)
+        {
+            if (e.ItemKind != LongListSelectorItemKind.Item)
+                return;
+            var item = e.Container.Content as ItemViewModel;
+            if (!App.ViewModel.IsDataLoading && ItemsList.ItemsSource[0] == item)
+            {
+                //if (!_firstRealized)
+                //{
+                //    _firstRealized = true;
+                //}
+                //else
+                //{
+                App.ViewModel.LoadTopElements(10);
+                    Debug.WriteLine("");
+                //}
+            }
+        }
+
+        private void ItemsList_OnItemUnrealized(object sender, ItemRealizationEventArgs e)
+        {
+            if (e.ItemKind != LongListSelectorItemKind.Item)
+                return;
+            var item = e.Container.Content as ItemViewModel;
+            if (!App.ViewModel.IsDataLoading && ItemsList.ItemsSource[0] == item)
+            {
+                    Debug.WriteLine("");
+            }
+        }
+    }
+}
---
+++ TestLongList/ViewModels/MainViewModel.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using Microsoft.Phone.Controls;
+using TestLongList.Resources;
+
+namespace TestLongList.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        //private WP8PullDetector _detector;
+
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty
+        {
+            get
+            {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        public bool IsDataLoading
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+            //LoadTopElements();
+            return;
+            // Sample data; replace with real data
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime one", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime two", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime three", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime four", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime five", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime six", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime seven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime eight", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime nine", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime ten", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime eleven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime twelve", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime thirteen", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime fourteen", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime fifteen", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            this.Items.Add(new ItemViewModel() { LineOne = "runtime sixteen", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void LoadBottomElements()
+        {
+            IsDataLoading = false;
+            int i = 90;
+            while (i < 100)
+            {
+                this.Items.Add(new ItemViewModel()
+                    {
+                        LineOne = i + " -- runtime one",
+                        LineTwo = "Maecenas praesent accumsan bibendum",
+                        LineThree =
+                            "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu"
+                    });
+                i++;
+            }
+            IsDataLoading = true;
+        }
+
+        private int counter = 0;
+        public void LoadTopElements(int count)
+        {
+            int i = 0;
+            while (i < count)
+            {
+                this.Items.Insert(0, new ItemViewModel()
+                    {
+                        LineOne = counter + " -- runtime one",
+                        LineTwo = "Maecenas praesent accumsan bibendum",
+                        LineThree =
+                            "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu"
+                    });
+                counter++;
+                i++;
+            }
+        }
+
+        //public void SetPullDetector(LongListSelector itemsList)
+        //{
+        //    _detector = new WP8PullDetector();
+        //    _detector.Bind(itemsList);
+        //    _detector.Compression += DetectorOnCompression;
+        //}
+
+        //private void DetectorOnCompression(object sender, CompressionEventArgs compressionEventArgs)
+        //{
+        //    if (compressionEventArgs.Type == CompressionType.Top)
+        //    {
+        //        //LoadTopElements();
+        //        Debug.WriteLine("");
+        //    }
+        //}
+    }
+}
